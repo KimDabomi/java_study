7e98bfe483e6bf4048c8dfe48d381fc5c3280c95
KimDabomi <cdabomi@nate.com>
2023-09-26 20:38:03 +0900

    chap02

---
+++ src/main/java/doit/java/example/chap02/IntArrayInit.java
@@ -0,0 +1,11 @@
+package doit.java.example.chap02;
+
+class IntArrayInit {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
---
+++ src/main/java/doit/java/practice/chap02/ReverseCopyArray.java
@@ -0,0 +1,37 @@
+package doit.java.practice.chap02;
+
+import java.util.Scanner;
+
+class ReverseCopyArray {
+    static void copy(int[] a, int[] b) {
+        int num = a.length <= b.length ? a.length : b.length;
+        for (int i = 0; i < num; i++)
+            a[num - i] = b[i];
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("a의 요솟수는  : ");
+        int numA = stdIn.nextInt();		// 요솟수
+        int[] a = new int[numA];			// 요솟수가 numa인 배열
+        for (int i = 0; i < numA; i++) {
+            System.out.print("a[" + i + "] : ");
+            a[i] = stdIn.nextInt();
+        }
+
+        System.out.print("b의 요솟수는  : ");
+        int numB = stdIn.nextInt();
+        int[] b = new int[numB];
+        for (int i = 0; i < numB; i++) {
+            System.out.print("b[" + i + "] : ");
+            b[i] = stdIn.nextInt();
+        }
+
+        copy(a, b);
+
+        System.out.println("배열 b의 모든 요소를 배열 a에 copy 하였습니다.");
+        for (int i = 0; i < numA; i++)
+            System.out.println("a[" + i + "] = " + a[i]);
+    }
+}
